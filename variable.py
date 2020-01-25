0a5c7c2b322c3739e8d938f0a13490c2114d1e6e
zaracky <51820857+zaracky@users.noreply.github.com>
2020-01-25 10:39:42 -0800

    Ajout du fichier variable

---
+++ variable.py
@@ -0,0 +1,20 @@
+#Ce fichier est utiliser pour definir automatiquement les variables au sein du script principal.
+#/!\ Ce fichier doit être sécuriser! Il est déconseiller de changer le nom des variables
+
+
+#On definit ici l'adresse qui sera utilisé pour expedier les mail
+expediteur= "mail expediteur"
+#Suivi du mot de passe
+mdp ="test"
+
+
+#On definit ensuite les variables necessaires à la création de la sauvegarde:
+
+#Ici l'endroit ou l'on souhaite sauvegarder nos fichiers
+localisation="/home/administrateur/sauvegarde"
+
+#Et enfin les informations du serveur (ip + endroit ou l'on souhaite avoir la sauvegarde
+ip="192.168.1.1"
+choice="y"
+access="/home/administrateur"
+
