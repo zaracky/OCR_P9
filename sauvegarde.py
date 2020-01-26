83f10494a1b435732dbc6a56fd5082f080f791d8
zaracky <51820857+zaracky@users.noreply.github.com>
2020-01-26 13:56:57 -0800

    Ajustement des fonctions
    Ajustement sauvegarde auto, restauration, et envoimail

--- sauvegarde.py
+++ sauvegarde.py
@@ -4,7 +4,13 @@ import smtplib
 import subprocess 
 from os import chdir, mkdir
 import datetime
-import sys
+
+#Ces librairies permettent l'envoie par mail
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
 
 #Début de la fonction menu
 def menu():
@@ -15,7 +21,7 @@ def menu():
 	print("-------------------------------------------------------------------")
 
 	print(" \n Votre choix:")
-#Si un argument est entrée on le selectionne en tant que choix (par exemple pour automatiser l'exécution
+#Si un argument est entrée on le selectionne en tant que choix (par exemple pour automatiser l'exécution)
 	if len( sys.argv ) > 1:
 		choice = sys.argv[1]
 	else:
@@ -102,82 +108,100 @@ def sauvegarde():
 
 #Debut de la fonction de sauvegarde créer pour l'automatisation
 def sauvegarde_auto():
-	#On definit le jour dans une variable qui sera utilise plus tard dans la fonction
+
+	global expediteur,mdp,mail
+
+#On verifie la précense du fichier variable.py
+	print("Verification de la présence du fichier variable.py en cours.. \n")
+	if os.path.isfile('variable.py'):
+		print("Le fichier variable.py est présent :) Début de la procédure...")
+#Si le fichier est présent on importe les variable
+		from variable import expediteur,mdp,localisation,ip,access
+#Sinon on fait appel a la fonction mail pour avertir l'administrateur
+	else:
+		envoimail()
+#On definit le jour dans une variable qui sera utilise plus tard dans la fonction
 	heure= datetime.datetime.now()	
 	heure2= heure.date()
-	print(" \n Entrer le repertoire ou la sauvegarde aura lieu:")
+#A la difference de la premier fonction aucune question ne vas etre posé
 	localisation="/home/administrateur/Bureau/sauvegarde"
-	#localisation= input("[exemple: /home/user/sauvegarde] >>")
-	print(" \n Entrer l'adresse Ip du serveur vers laquelle copier la sauvegarde: ")
 	ip="192.168.1.1"
-	#ip = input(" >>")
-	print("\n--------------------------------------------------------------------")
-	print("Recapitulatif des informations: \n Repertoire de sauvegarde: ",localisation,"\n Ip du serveur distant:",ip,"\n Date:",heure2)
-	print("--------------------------------------------------------------------")
+
 	#os.system('ssh administrateur@'+ip+' find /home/administrateur/test -type d -mtime +1 -exec echo {} \;')
-	print(" \n Etes vous sur?(y/n)")
-	choice = "y"
-	#choice = input(" >>")
-
-	if choice=="y":
-		print("Début de la sauvegarde...")
-		if not os.path.exists(localisation):
- 			os.makedirs(localisation)
-		os.chdir(localisation)
-		if not os.path.exists(str(heure2)):
- 			os.makedirs(str(heure2))
-		os.chdir(str(heure2))
-		os.system('sudo mysqldump  -u root   wordpress > sauv.sql')
-		os.system('sudo cp -r /var/www/html '+localisation+'/'+str(heure2))
-		os.system('crontab -u administrateur -l > crontab')
+	print("Début de la sauvegarde...")
+	if not os.path.exists(localisation):
+ 		os.makedirs(localisation)
+	os.chdir(localisation)
+	if not os.path.exists(str(heure2)):
+ 		os.makedirs(str(heure2))
+	os.chdir(str(heure2))
+#Meme proceder de sauvegarde que la fonction manuel
+	os.system('sudo mysqldump  -u root   wordpress > sauv.sql')
+	os.system('sudo cp -r /var/www/html '+localisation+'/'+str(heure2))
+	os.system('crontab -u administrateur -l > crontab')
 
 
-		print("Fichiers sauvegardé à l'emplacement suivant:",localisation)
-		print("\n Copie vers le serveur distant en cours..")
-		os.chdir(localisation)
-		os.system('scp -r '+str(heure2)+'/ administrateur@'+ip+':/home/administrateur')
-		print("\n Copie réalisé")
-		print("\n Analyse de la presence de sauvegarde superieur à 30jours..")
-		os.system('ssh administrateur@'+ip+' find /home administrateur/test -type d -mtime +10 -exec rm -fr {} \;')
+	print("Fichiers sauvegardé à l'emplacement suivant:",localisation)
+	print("\n Copie vers le serveur distant en cours..")
+	os.chdir(localisation)
+	os.system('scp -r '+str(heure2)+'/ administrateur@'+ip+':/home/administrateur')
+	print("\n Copie réalisé")
+	print("\n Analyse de la presence de sauvegarde superieur à 30jours..")
+	os.system('ssh administrateur@'+ip+' find /home/administrateur/test -type d -mtime +20 -exec rm -fr {} \;')
+	os.system('find /home/administrateur/Bureau/sauvegarde -type d -mtime +20 -exec rm -fr {} \;')
 #ssh login@Host 'find /home/exploit/ -size 0 -exec rm -i  {} \;'
 #find -type d -mtime +10
 
-		print("\n Retour vers le menu principal")
-		#input(" >>")
-		exit()
-	else:
-		sauvegarde()
+	print("\n Retour vers le menu principal")
 	return
+
 
 
 ####################################################################################
 
+
+#Debut de la fonction restauration
 def restauration() :
 	heure= datetime.datetime.now()	
 	heure2= heure.date()
+#On verifie/télécharge les pré-requis au fonctionnement du site
+	print("\n Téléchargement des pré-requis...")
+	os.system('sudo apt-get install mysql-server mysql-client')
+	os.system('sudo apt-get install apache2 php7.2 php7.2-mysql libapache2-mod-php7.2')
+	os.system('sudo service apache2 start ')
+#On averti l'utilisateur du résultat
+	print("\n Telechargement de tout les pré-requis réussis\n")
+
+#Pour procéder à la restauration de nombreux informations sont utiles:
 	print(" \n Entrer le repertoire ou la sauvegarde a eu lieu:")
+	#localisation= input("[exemple: /home/user/sauvegarde] >>")
 	localisation="/home/administrateur/Bureau/sauvegarde"	
 	print(" \n A quel date voulez-vous restaurez le site?")
 	jour= input("[exemple: 2019-12-16 pour 16 decembre 2019 ] >>")	
-	#localisation= input("[exemple: /home/user/sauvegarde] >>")
+
+#On verifie la présence des fichiers necessaires à la restauration
 	print("\n Verification de la présence des fichiers en cours..\n")
-	if os.path.isfile(localisation+'/'+jour+'/sauv.sql') and os.path.isdir(localisation+'/'+jour+'/wp-content'):
+	if os.path.isfile(localisation+'/'+jour+'/sauv.sql') and os.path.isdir(localisation+'/'+jour+'/html') and os.path.isdir(localisation+'/'+jour+'/crontab'):
 		print("Fichiers présents :) \n")
+#Si ils sont présent on peut débuter la restauration
 		print("Debut de la restauration..\n")
 		os.chdir(localisation+'/'+jour)
 		os.system('sudo mysql -u root   wordpress < sauv.sql')
-		os.system('sudo cp -r www.example.com/* /var/www/html/')
+		os.system('sudo crontab crontab ')
+		os.system('sudo cp -r html/* /var/www/html/')
 		os.system('sudo service apache2 restart ')
+#manque création bdb
 		print("Restauration terminée :) \n Retour au menu principal")
 		input(" >>")
 		menu()
 
-
+#Sinon on laisse la posibilité de le récuperer depuis un serveur distant
 	else:
-		print("\033[31m \n /_\ Erreur le repertoire de sauvegarde n'est pas présent!\n\033[0m")
+		print("\033[31m \n /_\ Erreur tout les fichiers ne sont pas présent!\n\033[0m")
 		print("Voulez-vous le recuperer à partir d'un serveur distant?(y/n)")
 		choice = input(" >>")
 		if choice=="y":
+#Si c'est le cas on demande les informations de connexion
 			print(" \n Entrer l'adresse Ip du serveur vers laquelle recuperer la sauvegarde: ")
 			ip = input(" >>")
 			print(" \n Entrer le repertoire ou se trouve la sauvegarde sur le serveur distant: ")
@@ -188,21 +212,31 @@ def restauration() :
 			print("\n--------------------------------------------------------------------")
 			print("Recapitulatif des informations: \n Repertoire de sauvegarde: ",repertoire,"\n Ip du serveur distant:",ip,"\n Date de restauration:",date2)
 			print("--------------------------------------------------------------------")
+#On demande confirmations des informations
 			print(" \n Etes vous sur?(y/n)")
 			choice = input(" >>")
 			if choice=="y":
+#Si oui début du telechargement et de la restauration
 				print(" \n Recuperation du fichier en cours..")
 				os.system('scp -r administrateur@'+ip+' '+repertoire+'/'+date2+' ' +localisation)
 				print("\n Recuperation terminée. Debut de la restauration..")
+#On verifie que tout les fichiers necessaires sont présent
+				if os.path.isfile(localisation+'/'+jour+'/sauv.sql') and os.path.isdir(localisation+'/'+jour+'/html') and os.path.isdir(localisation+'/'+jour+'/crontab'):
 
-				os.chdir(localisation+'/'+date2)
-				os.system('sudo mysql -u root   wordpress < sauv.sql')
-	
-				os.system('sudo cp -r www.example.com/* /var/www/html/ ')
-				os.system('sudo service apache2 restart ')
-				print("\n Restauration terminée :) \n Retour au menu principal")
-				input(" >>")
-				menu()
+					os.chdir(localisation+'/'+date2)
+					os.system('sudo mysql -u root   wordpress < sauv.sql')
+					os.system('sudo crontab crontab ')
+					os.system('sudo cp -r html/* /var/www/html/')
+					os.system('sudo service apache2 restart ')
+					print("\n Restauration terminée :) \n Retour au menu principal")
+					input(" >>")
+					menu()
+#Sinon on avertit l'utilisateur
+				else:
+					print("\033[31m \n /_\ Erreur les fichiers necessaires à la restauration ne sont pas présent!\n Retour au menu principal \n \033[0m")
+					input(" >>")
+					menu()
+
 #sudo scp -r administrateur@192.168.1.1:/home/administrateur/2019-12-07 /home/administrateur/Bureau/sauvegarde/
 			else  :	
 				os.system('clear')
@@ -216,55 +250,44 @@ def restauration() :
 
 
 	return
-
-	#os.chdir(localisation)
 	
 ########################################################################################
 
+def envoimail():
 
-def installation():
-	print("\n Début du téléchargement...")
-	os.system('sudo apt-get install mysql-server mysql-client ;echo valeur=$?>> erreur.py')
-	from erreur import valeur
-	if valeur != 0 :
-		print("\n\n\n /!\ Impossible de telecharger mysql (voir ligne ci dessus) \nMerci de verifier votre connexion internet.")
+	print("\nPréparation du mail pour l'envoie..")
+#On l'integre ensuite à une variable
+	mail= input(" >>")
 
-		os.system('rm erreur.py ')	
-		print(" \n Voulez-vous continuer?(y/n)")
-		choice = input(" >>")
-		if choice=="y":
-			print("\n Reprise du processus...")
-		else:
-			menu()
-	else:
-
-		print("Telechargement de Mysql réussi\n")
-		os.system('rm erreur.py ')
-
-	os.system('sudo apt-get install apache2 php7.2 php7.2-mysql libapache2-mod-php7.2 ;echo valeur=$?>> erreur.py')
-	from erreur import valeur
-	if valeur != 0 :
-		print("\n\n\n /!\ Impossible de telecharger apache (voir ligne ci dessus) \nMerci de verifier votre connexion internet.")
-		os.system('rm erreur.py ')	
-		print(" \n Voulez-vous continuer?(y/n)")
-		choice = input(" >>")
-		if choice=="y":
-			print("\n Reprise du processus...")
-		else:
-			menu()
-	else:
-		print("Telechargement de Apache réussi\n")
-		os.system('rm erreur.py ')
-		os.system('sudo service apache2 restart ')
-		print("Telechargement de tout les logiciels réussi\n")
-		print("Retour au menu principal")
-		input(" >>")
-		menu()
+	msg = MIMEMultipart()
+#On definit ici le corps du mail. Ce dernier sera a adapter en fonction du type de destinataire (technique ou non)
+	msg['From'] = expediteur
+	msg['To'] = mail
+	msg['Subject'] = '/!\ Alerte sauvegarde impossible' 
+	message = u"""\
+		Bonjour,
+		Une erreur est survenue concernant la sauvegarde du site web. Merci de lancer une sauvegarde manuelle afin d'analyser le message d'erreur
+		
+		Cordialement,
+		Le système d'information"""
+	msg.attach(MIMEText(message))
+#§§§§§§§§§§§§§§§ Serveur mail à modifier si l'on ne souhaite pas utiliser gmail
+	mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+	mailserver.ehlo()
+	mailserver.starttls()
+	mailserver.ehlo()
+#On indique les identifiants de connexion
+	mailserver.login(expediteur, mdp)
+	mailserver.sendmail(expediteur, mail, msg.as_string())
+	mailserver.quit()
+	print("\nEnvoie du mail terminé! Retour au menu principal")
 	return
 
 
 
-###################################################################################
+#################################################################################
+
+
 
 def readme():
 	mydb = mysql.connector.connect(
