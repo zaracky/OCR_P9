0516212669e9e8207e101bb8972c5f6d7c0c9efe
zaracky <51820857+zaracky@users.noreply.github.com>
2020-01-31 09:16:39 -0800

    Finalisation variable.py

--- variable.py
+++ variable.py
@@ -2,19 +2,23 @@
 #/!\ Ce fichier doit être sécuriser! Il est déconseiller de changer le nom des variables
 
 
-#On definit ici l'adresse qui sera utilisé pour expedier et recevoir les mail
+#On definit ici l'adresse qui sera utilisé pour expedier et recevoir les mail:
+
+#Pour ne pas avoir a modifier le script, merci d'utiliser une adresse gmail sinon voir la fonction mail
 expediteur= "mail expediteur"
 #Suivi du mot de passe
 mdp ="test"
 
+#Le mail destinataire de l'alerte
 mail="mail destinataire"
+
 
 #On definit ensuite les variables necessaires à la création de la sauvegarde:
 
-#Ici l'endroit ou l'on souhaite sauvegarder nos fichiers
+#Ici l'endroit ou l'on souhaite sauvegarder nos fichiers en local
 localisation="/home/administrateur/sauvegarde"
 
-#Et enfin les informations du serveur (ip + endroit ou l'on souhaite avoir la sauvegarde
+#Et enfin les informations du serveur (ip + endroit ou l'on souhaite avoir la sauvegarde sur le serveur
 ip="192.168.1.1"
 access="/home/administrateur"
 
